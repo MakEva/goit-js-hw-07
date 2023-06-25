718669770b5add1428647b8b64c7eca06caa8695
MakEva <sher.evikm@gmail.com>
2023-06-26 01:29:33 +0300

    first check

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const ulGallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+ulGallery.insertAdjacentHTML("beforeend", createItemMarkup(galleryItems));
+
+ulGallery.addEventListener("click", imgOpenModal);
+
+function createItemMarkup(arr) {
+    return arr.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                 <img class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                 />
+            </a>
+        </li>`
+    }).join("")
+}
+
+function imgOpenModal(evt) {
+    evt.preventDefault()
+    const cardItem = evt.target.closest(".gallery__image");
+        if (cardItem) {
+        const obj = findCurrentImg(cardItem);
+          const instance = basicLightbox.create(createModal(obj));
+            instance.show();
+            document.addEventListener("keydown", hendlerEscKey);
+           
+            function hendlerEscKey(e) {
+                if (e.code === "Escape") {
+                    instance.close();
+                }
+            }
+    }
+}
+
+function findCurrentImg(cardItem) {
+    const source = cardItem.dataset.source;
+    const currentImg = galleryItems.find(({ original }) => original === source);
+    return currentImg;
+}
+
+function createModal({ original, description } = {}) {
+     return `<div>
+             <img src="${original}" alt="${description}"/>
+          </div>` 
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulGallery = document.querySelector(".gallery");
+
+ulGallery.insertAdjacentHTML("beforeend", createItemMarkup(galleryItems));
+
+ulGallery.addEventListener("click", imgOpenModal);
+
+function createItemMarkup(arr) {
+    return arr.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+    }).join("")
+}
+
+function imgOpenModal(evt) {
+    evt.preventDefault();
+    let gallery = new SimpleLightbox(".gallery a",
+        {   captionsData: "alt",
+            captionPosition: 'bottom',
+            captionDelay: 250
+        });
+    gallery.on('show.simplelightbox', () => createModal());
+}
+    
+function createModal({ original, description } = {}) {
+     return `<div>
+             <img src="${original}" alt="${description}"/>
+          </div>` 
+}
+
